31bd00747a739821adfd946404ef04535e58941b
Siri-Kunte <sirikunte@gmail.com>
2022-11-12 10:19:34 +0530

    Displaying numbers from -10 to -1.py

---
+++ Displaying numbers from -10 to -1.py
@@ -0,0 +1,7 @@
+#Displaying the numbers from -10 to -1
+start_point = int(input("Enter the start point:"))
+endpoint = int(input("Enter the end point:"))
+num = start_point
+for i in range(start_point,endpoint):
+   num=num+1
+   print(num)
