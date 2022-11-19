1632c89b24e7919e6f9af94368a6e2da13b7b0ed
Siri-Kunte <sirikunte@gmail.com>
2022-11-19 16:56:38 +0530

    Continue statements

---
+++ Continue statements.py
@@ -0,0 +1,9 @@
+#While loop
+number=0
+end_point=int(input("Enter the end point:"))
+while number < end_point:
+    number=number+1
+    if number % 5==0:
+        print(number)
+    else:
+        continue
